03a9f663d58d66a243f7052c001c164e1c7024d3
cipheroot <cipheroot@proton.me>
2026-03-04 21:20:01 +0530

    fix vite config missing imports

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,5 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
